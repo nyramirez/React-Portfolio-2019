25d872b51c59d297a119b6262e50dd9bce3058b9
Nefas <nyramirez@gmail.com>
2019-12-30 17:40:37 -0700

    resume page finished

--- src/components/NrpResumeSubHeader.js
+++ src/components/NrpResumeSubHeader.js
@@ -1,39 +1,152 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { CardMedia } from '@material-ui/core';
 
-const NrpResumeSubHeader = () => (
-    <div className="subHeader content-container">
-        <div className="subHeader__content">
-            <Link className="subHeader__title" to="/objective">
-                <h5>OBJECTIVE & QUALIFICATIONS</h5>
-            </Link>
-            <Link className="subHeader__title" to="/work_experience">
-                <h5>PROFESSIONAL EXPERIENCE</h5>
-            </Link>
-            <Link className="subHeader__title" to="/education">
-                <h5>EDUCATION</h5>
-            </Link>
+
+const NrpResumeSubHeader = () => {
+
+    return (
+        <div className="content-container subHeader">
+            <div className="subHeader__content">
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/objective">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/goals-picture.jpg"
+                                    title="Target Picture"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">OBJECTIVE & QUALIFICATIONS</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/work_experience">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/profeesionaExp-picture.jpg"
+                                    title="team working"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">PROFESSIONAL EXPERIENCE</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/education">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/education-picture.jpg"
+                                    title="shadow of a graduated student"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">EDUCATION</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+            </div>
+
+            <div className="subHeader__content">
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/certification">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/certification-picture.jpg"
+                                    title="comparing stones in a wall"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">CERTIFICATIONS</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/technical_skills">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/techSkills-picture.jpg"
+                                    title="owl, device, bird of prey, figure, headphones, figurine, funny, workplace"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">TECHNICAL SKILLS</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/soft_skills">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/softSkills-picture.jpg"
+                                    title="young man with process map in whiteboard"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">SOFT SKILLS</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="subHeader__content">
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/projects">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/projects-picture.jpg"
+                                    title="woman with idea, plan action sign"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">COMPLETED PROJECTS</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+                <Card className="style__card">
+                    <CardContent>
+                        <Link className="subHeader__title" to="/references">
+                            <div className="image-container">
+                                <CardMedia
+                                    className="image__resume-cards"
+                                    image="/images/reference-picture.jpg"
+                                    title="group of volunteers with thumbs up"
+                                />
+                            </div>
+                            <div className="text-container">
+                                <h5 className="objText">REFERENCES</h5>
+                            </div>
+                        </Link>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
-        <div className="subHeader__content">
-            <Link className="subHeader__title" to="/certification">
-                <h5>CERTIFICATIONS</h5>
-            </Link>
-            <Link className="subHeader__title" to="/technical_skills">
-                <h5>TECHNICAL SKILLS</h5>
-            </Link>
-            <Link className="subHeader__title" to="/soft_skills">
-                <h5>SOFT SKILLS</h5>
-            </Link>
-        </div>
-        <div className="subHeader__content">
-            <Link className="subHeader__title" to="/projects">
-                <h5>COMPLETED PROJECTS</h5>
-            </Link>
-            <Link className="subHeader__title" to="/references">
-                <h5>REFERENCES</h5>
-            </Link>
-        </div>
-    </div>
-);
+    )
+};
+
 
 export default NrpResumeSubHeader;
--- src/components/resumeObjective.js
+++ src/components/resumeObjective.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import Header from './Header';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { CardMedia } from '@material-ui/core';
+
 
 const resumeObjective = () => {
-    return(
+
+    return (
         <div>
-            <Header/>
-
-            
-
-            <h1>OBJECTIVE & QUALIFICATIONS</h1>
-            <p>Professional Industrial Engineer with a profound understanding of project management principles, 
-            manufacturing and logistics best practices, plus web development. Excellent interpersonal and written 
-            communication skills with the ability to relate effectively with individuals at all levels. Trained and 
-            experienced in project management (PMI), lean manufacturing and six sigma terminology. Former process 
-            improvement engineer highly organized, analytical, and decisive with strong problem-solving capabilities. 
-            Teams under my supervision have demonstrated to be able to accurately perform challenging tasks with precision 
-            and attention to detail while at the same time keeping a strong customer-service orientation. I am driven to 
-            accomplish and surpass goals, highly competitive and possess strong teamwork skills.</p>
-            
+            <Header />
+            <div className="content-container content-setting">
+                <Card className="main-page-card">
+                    <CardContent>
+                        <div className="image-container">
+                            <CardMedia
+                                className="objective-pic"
+                                image="/images/goals-picture.jpg"
+                                title="Target Picture"
+                            />
+                        </div>
+                        <div className="text-container">
+                            <h1 className="objTitle">OBJECTIVE & QUALIFICATIONS</h1>
+                            <p className="objText">Professional Industrial Engineer with a profound understanding of project management principles,
+                            manufacturing and logistics best practices, plus web development. Excellent interpersonal and written
+                            communication skills with the ability to relate effectively with individuals at all levels. Trained and
+                            experienced in project management (PMI), lean manufacturing and six sigma terminology. Former process
+                            improvement engineer highly organized, analytical, and decisive with strong problem-solving capabilities.
+                            Teams under my supervision have demonstrated to be able to accurately perform challenging tasks with precision
+                            and attention to detail while at the same time keeping a strong customer-service orientation. I am driven to
+                        accomplish and surpass goals, highly competitive and possess strong teamwork skills.</p>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
         </div>)
 }
 
